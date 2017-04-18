e1b017fe006fe8f8fe7abc15098a8c6d2db2c749
nirakka <ikoa.syo@gmail.com>
2017-04-18 16:53:27 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp');
+
+var jade = require('gulp-jade-php');
+
+var plumber = require("gulp-plumber");
+var sass = require("gulp-sass");
+var bourbon= require("node-bourbon");
+var neat = require("node-neat");
+var notify = require("gulp-notify");
+var riot = require("gulp-riot");
+
+//var browser = require("browser-sync");
+gulp.task("scss", function(){ 
+  return gulp.src("./scss/*.scss")
+
+    .pipe(plumber({
+      errorHandler: notify.onError("Error: <%= error.message %>") //<-
+    }))
+
+    .pipe(sass({
+      includePaths: neat.includePaths
+    }).on('error', sass.logError)) 
+    .pipe(gulp.dest("./"));
+
+});
+
+
+gulp.task("riot", function(){ 
+  return gulp.src("./riottags/*.jade")
+    .pipe(riot( {
+      template:"pug"
+    }))
+    .pipe(gulp.dest("./public/riotjs/"));
+});
+
+gulp.task('watch',function () {
+  gulp.watch("./scss/**/*.scss", ["scss"]);
+  gulp.watch("./riottags/*.jade", ["riot"]);
+  gulp.watch(['./public/stylesheets/*.css'], function(event){
+    console.log("css changed");
+  });
+  gulp.watch('./source/**/*.jade', ['templates']);
+});
+
+gulp.task('templates', function() {
+  gulp.src('./source/**/*.jade')
+    .pipe(jade({
+        locals: {
+          title: 'OMG THIS IS THE TITLE'
+        }
+     }))
+     .pipe(gulp.dest('./'));
+});
+
+gulp.task('default', ['scss', 'riot', 'watch', 'templates']);
---
+++ index.php
@@ -0,0 +1,4 @@
+<html><head><title>OMG THIS IS THE TITLE</title></head></html><body><div><?php echo "waa";
+echo "aaa";var_dump();
+var_dump();
+var_dump();?><?php echo "aa"; ?></div><div class="aho"></div></body>

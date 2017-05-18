494449def72557a2ad6465f29d50d269aeca545e
Masafumi Ishii <icymasa@gmail.com>
2017-05-18 10:36:30 +0000

    added bourbon/neat/base and grid settings

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,6 @@ var jade = require('gulp-jade-php');
 
 var plumber = require("gulp-plumber");
 var sass = require("gulp-sass");
-var bourbon= require("node-bourbon");
-var neat = require("node-neat");
 var notify = require("gulp-notify");
 var riot = require("gulp-riot");
 var sourcemaps = require("gulp-sourcemaps");
@@ -13,14 +11,11 @@ var sourcemaps = require("gulp-sourcemaps");
 //var browser = require("browser-sync");
 gulp.task("scss", function(){ 
   return gulp.src("./scss/*.scss")
-
     .pipe(plumber({
       errorHandler: notify.onError("Error: <%= error.message %>") //<-
     }))
-
     .pipe(sourcemaps.init())
       .pipe(sass({
-        includePaths: neat.includePaths,
         sourceComments:true
       }).on('error', sass.logError)) 
     .pipe(sourcemaps.write("./"))

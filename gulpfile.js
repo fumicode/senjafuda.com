ddb9bd619fb655b4e6e94fd168c626f7b773e6df
忍者 <ninja@ninjanoMacBook-Pro.local>
2017-10-21 20:59:09 +0900

    added image directory to src.

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,11 @@ var jade = require('gulp-jade-php');
 var plumber = require("gulp-plumber");
 var sass = require("gulp-sass");
 var notify = require("gulp-notify");
-var riot = require("gulp-riot");
+//var riot = require("gulp-riot");
 var sourcemaps = require("gulp-sourcemaps");
 
 gulp.task("scss", function(){ 
-  return gulp.src("./scss/*.scss")
+  return gulp.src("src/scss/*.scss")
     .pipe(plumber({
       errorHandler: notify.onError("SCSS Error: <%= error.message %>") //<-
     }))
@@ -17,30 +17,9 @@ gulp.task("scss", function(){
       .pipe(sass({
         sourceComments:true
       }).on('error', sass.logError)) 
-    .pipe(sourcemaps.write("dest/"))
+    .pipe(sourcemaps.write("./"))
     .pipe(gulp.dest("dest/"));
 
-});
-
-
-/*
-gulp.task("riot", function(){ 
-  return gulp.src("./riottags/*.jade")
-    .pipe(plumber({
-      errorHandler: notify.onError("Riot Error: <%= error.message %>") //<-
-    }))
-    .pipe(riot( {
-      template:"pug"
-    }))
-    .pipe(gulp.dest("./riotjs/"));
-});
-*/
-
-
-gulp.task('watch',function () {
-  gulp.watch("src/scss/**/*.scss", ["scss"]);
-  //  gulp.watch("src/riottags/**/*.jade", ["riot"]);
-  gulp.watch('src/jade/**/*.jade', ['jade-php']);
 });
 
 gulp.task('jade-php', function() {
@@ -54,5 +33,39 @@ gulp.task('jade-php', function() {
     .pipe(gulp.dest('dest/'));
 });
 
-gulp.task('default', ['scss', 'riot', 'watch', 'jade-php']);
+gulp.task('images', function() {
+  gulp.src('src/images/**/*')
+    .pipe(gulp.dest('dest/images/'));
+});
 
+
+
+/*
+gulp.task("riot", function(){ 
+  return gulp.src("riottags/*.jade")
+    .pipe(plumber({
+      errorHandler: notify.onError("Riot Error: <%= error.message %>") //<-
+    }))
+    .pipe(riot( {
+      template:"pug"
+    }))
+    .pipe(gulp.dest("./dest/javascripts/riotjs/"));
+});
+*/
+
+
+gulp.task('function-php',function () {
+  return gulp.src("src/php/function.php")
+    .pipe(gulp.dest("dest/"));
+});
+
+gulp.task('watch',function () {
+  //はじめに./をつけてはならない
+  gulp.watch("src/scss/**/*.scss", ["scss"]);
+  //  gulp.watch("src/riottags/**/*.jade", ["riot"]);
+  gulp.watch('src/jade/**/*.jade', ['jade-php']);
+  gulp.watch('src/php/function.php', ['function-php']);
+  gulp.watch('src/images/**/*', ['images']);
+});
+
+gulp.task('default', ['scss', 'watch', 'jade-php', 'function-php', 'images' ]);
